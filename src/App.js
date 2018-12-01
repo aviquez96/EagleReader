02ebea2ace4cf4bc9539083c672ef8fc27b1df45
jluisfgarza <jluisfgarza@gmail.com>
2018-12-01 13:27:00 -0800

    landing init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Landing from "./page/landing/landing"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hellooo
+        <Landing/>
       </div>
     );
   }
--- src/page/landing/landing.jsx
+++ src/page/landing/landing.jsx
@@ -1,18 +1,66 @@
 import React, { Component, Fragment } from "react";
-import section from "./sections";
-import bg from "";
+import PropTypes from "prop-types";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
 
-export class landing extends Component {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    overflow: "hidden",
+    padding: `0 ${theme.spacing.unit * 3}px`
+  },
+  paper: {
+    // maxWidth: 400,
+    height: "25vh",
+    margin: `${theme.spacing.unit}px auto`,
+    padding: theme.spacing.unit * 2
+  }
+});
+const message = `Truncation should be conditionally applicable on this long line of text
+ as this is a much longer line than what the container can support. `;
+
+export class Landing extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <Fragment>
-        <div>Landing</div>
-        <section img={bg} title={"resume book"} />
-        <section img={bg} title={"voice commands"} />
-        <section img={bg} title={"toggle sound"} />
+        <div className={classes.root}>
+          <Paper className={classes.paper}>
+            <Grid container wrap="nowrap" spacing={16}>
+              <Grid item xs zeroMinWidth>
+                <Typography noWrap>{"resume"}</Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+          <Paper className={classes.paper}>
+            <Grid container wrap="nowrap" spacing={16}>
+              <Grid item xs>
+                <Typography noWrap>{"library"}</Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+          <Paper className={classes.paper}>
+            <Grid container wrap="nowrap" spacing={16}>
+              <Grid item xs={4} md={4} lg={4}>
+                <Typography>{message}</Typography>
+              </Grid>
+              <Grid item xs={4} md={4} lg={4}>
+                <Typography>{message}</Typography>
+              </Grid>
+              <Grid item xs={4} md={4} lg={4}>
+                <Typography>{message}</Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        </div>
       </Fragment>
     );
   }
 }
-
-export default landing;
+Landing.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+export default withStyles(styles)(Landing);
